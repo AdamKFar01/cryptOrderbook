8ab58490cc0fb0e4b7380367132be3e4d3149270
Adam Farhat <akfarhat05@gmail.com>
2026-07-03 21:15:51 +0100

    Added OrderBook class with applyUpdate logic for bids and asks

---
+++ include/OrderBook.h
@@ -0,0 +1,65 @@
+//
+// Created by Adam Farhat on 03/07/2026.
+//
+
+#ifndef CRYPTORDERBOOK_ORDERBOOK_H
+#define CRYPTORDERBOOK_ORDERBOOK_H
+
+#include <map>
+#include <vector>
+#include <cstdint>
+#include <functional>
+
+class OrderBook {
+public:
+
+    // Loads the full starting order book
+    void applySnapshot() {
+        // empty for now
+    }
+
+
+    // Applies changes to the current order book
+    void applyUpdate(const std::vector<std::pair<double, double>>& bids,
+                     const std::vector<std::pair<double, double>>& asks) {
+
+        // Update bid prices
+        for (const auto& bid : bids) {
+            double price = bid.first;
+            double qty = bid.second;
+
+            if (qty == 0) {                      // If qty is 0, remove this price level
+                bid_book.erase(price);
+            } else {                             // Otherwise, add or update this price level
+                bid_book[price] = qty;
+            }
+        }
+
+        // Update ask prices
+        for (const auto& ask : asks) {
+            double price = ask.first;
+            double qty = ask.second;
+
+            if (qty == 0) {                     // If qty is 0, remove this price level
+                ask_book.erase(price);
+            } else {
+                ask_book[price] = qty;          // Otherwise, add or update this price level
+            }
+        }
+    }
+
+    // Calculates checksum to check if the book is correct
+    uint32_t calculateChecksum() {
+        // empty for now
+        return 0;
+    }
+
+private:
+    // Bids sorted from highest price to lowest price
+    std::map<double, double, std::greater<double>> bid_book;
+
+    // Asks sorted from lowest price to highest price
+    std::map<double, double> ask_book;
+};
+
+#endif //CRYPTORDERBOOK_ORDERBOOK_H
--- main.cpp
+++ main.cpp
@@ -1,12 +1,69 @@
-//
 // Created by Adam Farhat on 03/07/2026.
-//
+// v2 snapchat and update messages link: https://docs.kraken.com/exchange/api-reference/spot-websocket-v2/book
 
+
+#include <ixwebsocket/IXNetSystem.h>
+#include <ixwebsocket/IXWebSocket.h>
+#include <ixwebsocket/IXUserAgent.h>
 #include <iostream>
+#include <thread>
+#include <chrono>
 
 int main () {
 
-    std::cout << "Yo" << std::endl;
+    // Needed for windows, probably won't be of use for mac
+    ix::initNetSystem();
+
+    // Creating WebSocket and address that will be used for connection
+    ix::WebSocket webSocket;
+    std::string url("wss://ws.kraken.com/v2");
+    webSocket.setUrl(url);
+
+    // Defining what should automatically happen in each upcoming scenario
+    webSocket.setOnMessageCallback([](const ix::WebSocketMessagePtr& msg) {
+
+        if (msg->type == ix::WebSocketMessageType::Message)     // if a message arrives
+        {
+            std::cout << "Received message: " << msg->str << std::endl;
+            std::cout << "> " << std::flush;
+        }
+        else if (msg->type == ix::WebSocketMessageType::Open)   // if a connection opens
+        {
+            std::cout << "Connection established!" << std::endl;
+            std::cout << "> " << std::flush;
+        }
+        else if (msg->type == ix::WebSocketMessageType::Error)  // If an error occurs
+        {
+            // Maybe SSL is not configured properly
+            std::cout << "Connection error: " << msg->errorInfo.reason << std::endl;
+            std::cout << "> " << std::flush; // flush forces the cout output to show immediately
+        }
+
+    }
+    );
+
+    webSocket.start();                  // A session is started
+    //webSocket.send("Yo");               // A message is sent
+    std::cout << "> " << std::flush;    // Display a prompt
+    std::string text;                   // A string of text
+
+
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    /*
+    // This loop reads what is typed in the terminal, line by line
+    // And sends each line as a new message
+    // Not needed because we will be reading stuff from Kraken, not user input
+    while (std::getline(std::cin, text)) {
+
+        webSocket.send(text);
+        std::cout << "> " << std::flush;
+
+    }
+    */
+
     return 0;
 
 }
